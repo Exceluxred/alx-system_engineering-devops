a0a6f4be63ac50663f69665f7ca54d7934bfb924
Exceluxred <excelartmusic@gmail.com>
2023-03-31 16:07:33 +0100

    all

---
+++ 0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This manifest creates a file at /tmp
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
