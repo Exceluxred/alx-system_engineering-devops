fb1bd68998ce6c1facd660f759850ff60d1274d5
exceluxred <excelartmusic@gmail.com>
2023-04-27 16:49:22 +0100

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,29 +4,41 @@ from sys import argv
 
 
 if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: {} <employee_id>".format(argv[0]))
+        exit(1)
+
     _id = argv[1]
 
     base_url = "https://jsonplaceholder.typicode.com/"
     user_url = "{}users/{}".format(base_url, _id)
     tasks_url = "{}todos?userId={}".format(base_url, _id)
 
-    response = requests.get(user_url)
-    use_info = response.json()
-    name = use_info["name"]
+    try:
+        response = requests.get(user_url)
+        response.raise_for_status()
+        use_info = response.json()
+        name = use_info["name"]
 
-    res = requests.get(tasks_url)
-    tasks = res.json()
+        res = requests.get(tasks_url)
+        res.raise_for_status()
+        tasks = res.json()
 
-    done_tasks = 0
-    total_tasks = 0
-    completed_titles = []
-    for task in tasks:
-        if task["completed"]:
-            done_tasks += 1
-            completed_titles.append(task["title"])
-        total_tasks += 1
+        done_tasks = 0
+        total_tasks = 0
+        completed_titles = []
+        for task in tasks:
+            if task["completed"]:
+                done_tasks += 1
+                completed_titles.append(task["title"])
+            total_tasks += 1
 
-    print("Employee {} ".format(name), end="")
-    print("is done with tasks({}/{}):".format(done_tasks, total_tasks))
-    for title in completed_titles:
-        print("\t {}".format(title))
+        print("Employee {} ".format(name), end="")
+        print("is done with tasks({}/{}):".format(done_tasks, total_tasks))
+        for title in completed_titles:
+            print("\t {}".format(title))
+
+    except requests.exceptions.RequestException as e:
+        print("Error: {}".format(e))
+        exit(1)
+
